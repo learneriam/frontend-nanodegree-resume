fc71a385146d9b792fa74aa44b33ec608130a82c
learneriam <negi.jyotsna@gmail.com>
2016-02-25 00:22:46 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,8 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-var myheadername = HTMLheaderName.replace("%data","Jyotsna Negi");
-var myheaderrole = HTMLheaderRole.replace("%data","PeopleSoft Developer");
+var myheadername = HTMLheaderName.replace("%data%","Jyotsna Negi");
+var myheaderrole = HTMLheaderRole.replace("%data%","PeopleSoft Developer");
 
 $("#header").append(myheadername);
 $("#header").append(myheaderrole);
