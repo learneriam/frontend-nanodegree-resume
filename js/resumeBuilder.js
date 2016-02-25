3dc2cb0ad8aa3dabd0a0a2c6821bd93bfea92675
learneriam <negi.jyotsna@gmail.com>
2016-02-24 23:51:55 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,8 +1,3 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */$("#main").append("Jyotsna Negi");
- 
- var awesomeThoughts = "I am Jyotsna. I am Awesome";
- var funThoughts= awesomeThoughts.replace("Awesome","Fun");
- 
- $("#main").append(funThoughts);
