84c97db9365c306d5aa065e4c23d5fac32c3360f
learneriam <negi.jyotsna@gmail.com>
2016-03-02 00:19:29 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,6 +4,7 @@ This is empty on purpose! Your code to build the resume will go here.
 var myheadername = HTMLheaderName.replace("%data%","Jyotsna Negi");
 var myheaderrole = HTMLheaderRole.replace("%data%","PeopleSoft Developer");
 var bio = "Bellevue - 98007, Phone Nbr: 2066195346";
+var img = "C:\Users\jyots\Desktop\Scrapbook\Box Pics\103APPLE\IMG_3480.jpg"
 $("#header").append(myheadername);
 $("#header").append(myheaderrole);
 $("#header").append(bio);
