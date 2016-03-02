b9d4190ba2781a0cfa60b9d459381a2960ac4e81
learneriam <negi.jyotsna@gmail.com>
2016-03-02 00:15:46 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,6 +3,7 @@ This is empty on purpose! Your code to build the resume will go here.
  */
 var myheadername = HTMLheaderName.replace("%data%","Jyotsna Negi");
 var myheaderrole = HTMLheaderRole.replace("%data%","PeopleSoft Developer");
-
+var bio = "Bellevue - 98007, Phone Nbr: 2066195346";
 $("#header").append(myheadername);
 $("#header").append(myheaderrole);
+$("#header").append(bio);
