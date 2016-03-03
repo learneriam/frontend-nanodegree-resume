3d2a33a98c05534a326958317e001c123d8d777d
learneriam <negi.jyotsna@gmail.com>
2016-03-02 20:34:14 -0500

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -7,5 +7,16 @@ var bio = "Bellevue - 98007, Phone Nbr: 2066195346";
 var img = "images\IMG_3480.jpg"
 $("#header").append(myheadername);
 $("#header").append(myheaderrole);
-$("#main").append(bio);
+$("#header").append(bio);
+
+var work ={curjob:'Senior PeopleSoft Developer', 
+  curemp:'Hughes Telecom',
+  curdur: 'Oct-2015 - Till date',
+  curcit: 'Washington, D.C.'
+};
+var edu={sch:'College of Technology, GBPUAT, India',
+  year : '2004-2008'
+  major: 'B.Tech, Electronics and Communications'
+};
 $("#header").append(img);
+$("#main").append(work.curjob);
